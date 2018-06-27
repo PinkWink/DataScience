72dcb504a306bd52e8c63f287985df737c317c2f
PinkWink <pinkwink.korea@gmail.com>
2018-06-27 10:02:02 +0900

    add noti

---
+++ source_code/test_click.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.plot(np.random.rand(10))
+
+def onclick(event):
+    print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+          ('double' if event.dblclick else 'single', event.button,
+           event.x, event.y, event.xdata, event.ydata))
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+plt.show()
